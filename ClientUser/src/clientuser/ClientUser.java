85e707ed4f634f1d729a49a1932aa9c32af5abe8
Panzeri Tommaso Ambrogio <panzeri.tommasoambro@osdbsesto>
2022-02-23 09:40:59 +0100

    first line of code

--- ClientUser/src/clientuser/ClientUser.java
+++ ClientUser/src/clientuser/ClientUser.java
@@ -4,7 +4,12 @@
  * and open the template in the editor.
  */
 package clientuser;
-
+import java.io.*;
+import java.net.*;
+import java.io.File;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author panzeri.tommasoambro
@@ -16,6 +21,29 @@ public class ClientUser {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+         try {
+            String user, password; 
+            Socket clientSocket = new Socket("10.1.33.200", 5000);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            Scanner tastiera = new Scanner(System.in);
+            String scelta= tastiera.next();
+            if(scelta.equals("log")){
+                System.out.println("inserisci user");
+                user=tastiera.next();
+                System.out.println("inserisci password");
+                password=tastiera.next();
+
+            } else {
+
+            }
+            in.close();
+            out.close();
+            clientSocket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ClientUser.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+
     
 }
--- ServerUser/src/serveruser/ServerUser.java
+++ ServerUser/src/serveruser/ServerUser.java
@@ -9,6 +9,11 @@ package serveruser;
  *
  * @author panzeri.tommasoambro
  */
+import java.net.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ServerUser {
 
     /**
@@ -16,6 +21,19 @@ public class ServerUser {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            System.out.println("vuoi registrare un nuovo utente o loggarti?");
+            in.close();
+            out.close();
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ServerUser.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+
 }
