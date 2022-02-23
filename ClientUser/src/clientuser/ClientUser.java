d682e02ed54bfea7278dcaee589e8c6639afa9fb
Panzeri Tommaso Ambrogio <panzeri.tommasoambro@osdbsesto>
2022-02-23 09:52:29 +0100

    creation file

--- ClientUser/src/clientuser/ClientUser.java
+++ ClientUser/src/clientuser/ClientUser.java
@@ -4,12 +4,14 @@
  * and open the template in the editor.
  */
 package clientuser;
+
 import java.io.*;
 import java.net.*;
 import java.io.File;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 /**
  *
  * @author panzeri.tommasoambro
@@ -21,19 +23,22 @@ public class ClientUser {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-         try {
-            String user, password; 
+        try {
+            String user, password;
             Socket clientSocket = new Socket("10.1.33.200", 5000);
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             Scanner tastiera = new Scanner(System.in);
-            String scelta= tastiera.next();
-            if(scelta.equals("log")){
+            String scelta = tastiera.next();
+            if (scelta.equals("log")) {
+                password = tastiera.next();
+                FileWriter w = new FileWriter("credenziali.txt");
                 System.out.println("inserisci user");
-                user=tastiera.next();
+                w.write(tastiera.next());
                 System.out.println("inserisci password");
-                password=tastiera.next();
-
+                w.write(tastiera.next());
+                w.flush();
+                out.print(w);
             } else {
 
             }
@@ -45,5 +50,4 @@ public class ClientUser {
         }
     }
 
-    
 }
